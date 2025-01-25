1482dc09671d13f2539f330aefd4b36098b977ca
Michel <michel0810.ma@gmail.com>
2025-01-25 15:12:12 -0600

    Api de venta de productos agregado

--- backend/routes/shoppingRoutes.js
+++ backend/routes/shoppingRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { comprarMembresia, renovarMembresia } from "../controllers/shoppingController.js";
+import { comprarMembresia, comprarProductos, renovarMembresia } from "../controllers/shoppingController.js";
 
 const router = express.Router();
 
 router.post("/membresia",comprarMembresia);
 router.post("/renovarMembresia",renovarMembresia);
+router.post("/productos",comprarProductos);
 export default router;
